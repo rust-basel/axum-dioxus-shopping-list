bee26f94b30069e7c1ae90286183e2cb74433968
yguenduez <yasinguenduez1@gmail.com>
2024-03-07 21:21:23 +0100

    add simple post endpoint for item creation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,24 @@
-use axum::{routing::get, Router};
-use axum::response::IntoResponse;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::routing::post;
+use axum::Json;
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use uuid::Uuid;
+
 use axum::http::HeaderMap;
-use model::ShoppingListItem;
+use axum::response::IntoResponse;
+use axum::{routing::get, Router};
+use model::{PostShopItem, PostShopItemResponse, ShoppingListItem};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let db = SharedData::default();
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/items", post(create_shopping_item))
+        .with_state(db);
 
-    // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
@@ -26,9 +36,54 @@ async fn handler() -> impl IntoResponse {
     (headers, serialized)
 }
 
+type SharedData = Arc<RwLock<InMemoryDatabase>>;
+
+#[derive(Default)]
+pub struct InMemoryDatabase {
+    db: HashMap<String, ShoppingItem>,
+}
+
+struct ShoppingItem {
+    title: String,
+    creator: String,
+}
+
+impl From<PostShopItem> for ShoppingItem {
+    fn from(value: PostShopItem) -> Self {
+        Self {
+            title: value.title,
+            creator: value.posted_by,
+        }
+    }
+}
+
 fn example_list() -> Vec<ShoppingListItem> {
     vec![
-        ShoppingListItem{title: String::from("Coffee"), posted_by: String::from("Roland") },
-        ShoppingListItem{title: String::from("Tomato seeds"), posted_by: String::from("Tania") }
+        ShoppingListItem {
+            title: String::from("Coffee"),
+            posted_by: String::from("Roland"),
+        },
+        ShoppingListItem {
+            title: String::from("Tomato seeds"),
+            posted_by: String::from("Tania"),
+        },
     ]
+}
+
+pub async fn create_shopping_item(
+    State(state): State<SharedData>,
+    Json(request): Json<PostShopItem>,
+) -> impl IntoResponse {
+    let item = ShoppingItem::from(request.clone());
+    let uuid = Uuid::new_v4().to_string();
+    state.write().unwrap().db.insert(uuid.clone(), item);
+
+    (
+        StatusCode::CREATED,
+        Json(PostShopItemResponse {
+            id: uuid,
+            posted_by: request.posted_by,
+            title: request.title,
+        }),
+    )
 }
