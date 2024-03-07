e4deba59d462e74c78e173c78fb523b993c1dd3e
rb <r.brand@iwf.ch>
2024-03-07 08:54:28 +0100

    fetch json instead of text from backend, using the common model as an interface

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{routing::get, Router};
 use axum::response::IntoResponse;
 use axum::http::HeaderMap;
+use model::ShoppingListItem;
 
 #[tokio::main]
 async fn main() {
@@ -18,5 +19,16 @@ async fn main() {
 async fn handler() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
     headers.insert("Access-Control-Allow-Origin", "*".parse().unwrap()); // CORS allow-all for localhost development
-    (headers, "Hello from the backend")
+
+    let result = example_list();
+    let serialized = serde_json::to_string(&result).unwrap();
+
+    (headers, serialized)
+}
+
+fn example_list() -> Vec<ShoppingListItem> {
+    vec![
+        ShoppingListItem{title: String::from("Coffee"), posted_by: String::from("Roland") },
+        ShoppingListItem{title: String::from("Tomato seeds"), posted_by: String::from("Tania") }
+    ]
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,22 +1,24 @@
 #![allow(non_snake_case)]
-// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
+use model::ShoppingListItem;
 
 fn main() {
-    // launch the web app
     dioxus_web::launch(App);
 }
 
-// create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
 
     let backend_data = use_future(cx, (), |_| get_hello_world());
 
     match backend_data.value() {
-        Some(Ok(text)) => {
+        Some(Ok(items)) => {
             render! {
-                div {
-                    {text.clone()}
+                ul {
+                    for item in items {
+                        li {
+                           item.title.clone()
+                        }
+                    }
                 }
             }
         }
@@ -29,7 +31,10 @@ fn App(cx: Scope) -> Element {
     }
 }
 
-async fn get_hello_world() -> Result<String, reqwest::Error> {
-    let hello = reqwest::get("http://127.0.0.1:3000").await?.text();
-    hello.await
+async fn get_hello_world() -> Result<Vec<ShoppingListItem>, reqwest::Error> {
+    let list = reqwest::get("http://127.0.0.1:3000")
+        .await?
+        .json::<Vec<ShoppingListItem>>()
+        .await;
+    list
 }
