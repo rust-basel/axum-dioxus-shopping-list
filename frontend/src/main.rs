1e8da660bc5700ec1c4fb2d9d9cd1bac5d44172e
yguenduez <yasinguenduez1@gmail.com>
2024-03-23 10:18:37 +0100

    A new component serving to load or create a new list

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -16,6 +16,8 @@ const fn items_url() -> &'static str {
 #[derive(Routable, Clone)]
 enum Route {
     #[route("/")]
+    LoadOrCreateList {},
+    #[route("/list")]
     Home {},
     #[route("/profile")]
     Profile {},
@@ -49,6 +51,70 @@ fn Profile(cx: Scope) -> Element {
             }}
         }
     }
+}
+#[component]
+fn LoadOrCreateList(cx: Scope) -> Element {
+    let uuid = use_state(cx, || "9e137e61-08ac-469d-be9d-6b3324dd20ad".to_string());
+    let nav = use_navigator(cx);
+    let onloadsubmit = move |evt: FormEvent| {
+        cx.spawn({
+            let nav = nav.clone();
+            async move {
+                let uuid_value = evt.values["uuid"].first().cloned().unwrap_or_default();
+                // load list with given uuid
+
+                // if response ok - then route to /list/:uuid
+                nav.push(Route::Home {});
+            }
+        });
+    };
+
+    let on_create_list_click = move |_| {
+        let nav = nav.clone();
+        cx.spawn({
+            async move {
+                // make a post with new list -> receive an uuid
+
+                // if response ok - then route to /list/:uuid
+                nav.push(Route::Home {});
+            }
+        });
+    };
+
+    cx.render(rsx! {
+        ThemeChooserLayout{
+            div{
+                class: "grid place-items-center min-h-500",
+                div{
+                    class: "flex justify-content",
+                    button{
+                        class: "btn m-4",
+                        onclick: on_create_list_click,
+                        "Create new List"
+                    }
+                    form {
+                        onsubmit: onloadsubmit,
+                        div {
+                            class: "flex flex-col",
+                            button{
+                                class: "btn m-4",
+                                r#type: "submit",
+                                "Load existing List"
+                            }
+                            input{
+                                class:"input input-bordered",
+                                r#type:"text",
+                                placeholder:"Type here the uuid",
+                                id: "uuid",
+                                name: "uuid",
+                                oninput: move |e| uuid.set(e.value.clone())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
 }
 
 #[component]
