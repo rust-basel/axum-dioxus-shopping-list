834bdaa4be49160ee491bbea5d809937788cb2bd
yguenduez <yasinguenduez1@gmail.com>
2024-03-24 09:17:31 +0100

    backend: create databse module

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,9 @@
+mod database;
+
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::Json;
+use database::{InMemoryDatabase, ShoppingItem};
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use tower_http::cors::CorsLayer;
@@ -9,6 +12,8 @@ use uuid::Uuid;
 use axum::response::IntoResponse;
 use axum::{routing::delete, routing::get, Router};
 use model::{CreateListResponse, PostShopItem, PostShopItemResponse, ShoppingListItem};
+
+type SharedData = Arc<RwLock<InMemoryDatabase>>;
 
 #[tokio::main]
 async fn main() {
@@ -40,91 +45,6 @@ async fn get_items(Path(uuid): Path<Uuid>, State(state): State<SharedData>) -> i
     result.append(&mut items);
 
     Json(result)
-}
-
-type SharedData = Arc<RwLock<InMemoryDatabase>>;
-
-#[derive(Default)]
-pub struct ShoppingList {
-    list: HashMap<String, ShoppingItem>,
-}
-
-pub struct InMemoryDatabase {
-    db: HashMap<String, ShoppingList>,
-}
-
-impl Default for InMemoryDatabase {
-    fn default() -> Self {
-        let mut db = HashMap::new();
-        db.insert(
-            "9e137e61-08ac-469d-be9d-6b3324dd20ad".to_string(),
-            ShoppingList::default(),
-        );
-        InMemoryDatabase { db }
-    }
-}
-
-impl InMemoryDatabase {
-    fn insert_item(&mut self, list_uuid: String, item_uuid: String, shopping_item: ShoppingItem) {
-        self.db
-            .get_mut(&list_uuid)
-            .and_then(|list| list.list.insert(item_uuid, shopping_item));
-    }
-
-    fn get_item(&self, list_uuid: String, item_uuid: String) -> Option<&ShoppingItem> {
-        self.db
-            .get(&list_uuid)
-            .and_then(|list| list.list.get(&item_uuid))
-    }
-
-    fn delete_item(&mut self, list_uuid: String, item_uuid: String) {
-        self.db
-            .get_mut(&list_uuid)
-            .and_then(|list| list.list.remove(&item_uuid));
-    }
-
-    fn create_list(&mut self, list_uuid: String) {
-        self.db.insert(list_uuid, ShoppingList::default());
-    }
-
-    fn get_list(&self, list_uuid: String) -> Option<&ShoppingList> {
-        self.db.get(&list_uuid)
-    }
-
-    fn delete_list(&mut self, list_uuid: String) {
-        self.db.remove(&list_uuid);
-    }
-
-    fn as_vec(&self, list_uuid: String) -> Vec<ShoppingListItem> {
-        let list = self.get_list(list_uuid);
-        match list {
-            Some(list) => list
-                .list
-                .iter()
-                .map(|(key, item)| ShoppingListItem {
-                    title: item.title.clone(),
-                    posted_by: item.creator.clone(),
-                    uuid: key.clone(),
-                })
-                .collect(),
-            None => Vec::default(),
-        }
-    }
-}
-
-#[derive(Clone)]
-struct ShoppingItem {
-    title: String,
-    creator: String,
-}
-
-impl From<PostShopItem> for ShoppingItem {
-    fn from(value: PostShopItem) -> Self {
-        Self {
-            title: value.title,
-            creator: value.posted_by,
-        }
-    }
 }
 
 fn example_list() -> Vec<ShoppingListItem> {
